05aaa47675186cdd9fab57528abe840894402966
inescayolla <ines.cayolla@gmail.com>
2022-08-15 14:15:52 +0200

    solved

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,10 +1,19 @@
 class QuestionController < ApplicationController
-  def ask;
+  def ask;end
 
-    =begin if params[:question] == "I am going to work"
-      <%= "nothing" %> =end
+  def answer
+    if params[:question] == [:question.ends_with?("?")]
+      @answer = "Silly question, get dressed and go to work!"
+    elsif params[:question] == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
-    end
+  end
 
-  def answer; end
+
+
 end
+
+
+
