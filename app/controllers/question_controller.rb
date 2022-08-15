2c0a8b102f0500715e6e0b07da9e2ae9fa59c2a8
inescayolla <ines.cayolla@gmail.com>
2022-08-15 12:10:17 +0200

    s

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,10 @@
+class QuestionController < ApplicationController
+  def ask;
+
+    =begin if params[:question] == "I am going to work"
+      <%= "nothing" %> =end
+    end
+    end
+
+  def answer; end
+end
